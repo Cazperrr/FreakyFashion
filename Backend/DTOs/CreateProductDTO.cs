95fed9aeec2663db57390d4959033abbdb3876e8
Cazper <cazper>
2026-05-19 12:16:27 +0200

    Added DTO for Products

---
+++ Backend/DTOs/CreateProductDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace FreakyFashion.DTOs;
+
+public record CreateProductDTO(
+    int Id,
+    string Name,
+    string Description,
+    decimal Price,
+    string Image,
+    string UrlSlug,
+    int CategoryId
+);
---
+++ Backend/DTOs/ProductsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace FreakyFashion.DTOs;
+
+public record ProductsDTO(
+    int Id,
+    string Name,
+    string Description,
+    decimal Price,
+    string Image,
+    string UrlSlug
+);
