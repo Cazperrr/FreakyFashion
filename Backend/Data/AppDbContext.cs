2c29a2af7369b3974ca40362034eb745c401244f
Cazperrr <cazper.e@hotmail.com>
2026-05-29 11:56:27 +0200

    Added EF and DbContext

---
+++ Backend/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FreakyFashion.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreakyFashion.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Categories> Categories { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
